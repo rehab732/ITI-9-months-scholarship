daf47d849a2d849d44e471bc9f1a6612398bf5a7
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-03-06 04:19:08 +0200

    minor changes in day 1 and add day2 apply crud operation

---
+++ MVC/Day2/CarOperation/Controllers/CarController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Antlr.Runtime.Misc;
+using CarOperation.Models;
+namespace CarOperation.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Employee
+        public ActionResult GetAllCars()
+        {
+            List<Car> carslist = CarList.cars;
+            //step1 need to add view
+            //step2 pass data from controller to view by(viewdata or viewbag)
+            //let use viewbag
+            ViewBag.cars=carslist;//shared with me in view
+
+            return View();
+        }
+        public ActionResult SelectCarByID(int id) {
+
+            Car selectedcar= CarList.cars.FirstOrDefault(c=>c.Num==id);
+            ViewBag.selectedcar = selectedcar;
+
+            return View();
+        }
+        public ActionResult DeleteCarByID(int id) {
+
+            var deleted=CarList.cars.FirstOrDefault(c=>c.Num==id);
+             CarList.cars.Remove(deleted);
+
+            return RedirectToAction("GetAllCars");
+        }
+        public ActionResult EditCarByID(int id)
+        {
+            var editcar = CarList.cars.FirstOrDefault(c => c.Num == id);
+            ViewBag.edit = editcar;
+            return View();
+        }
+        public ActionResult  SaveCarData( int id,string model,string color,string manfacture) { 
+            Car saveCar= CarList.cars.FirstOrDefault(c=>c.Num== id);
+            saveCar.Model= model;
+            saveCar.Color=color;
+            saveCar.Manfacture= manfacture;
+            return RedirectToAction("GetAllCars");
+        }
+        public ActionResult CreateNewCar() {
+            Car car= new Car();
+            ViewBag.create = car;
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult CreateNewCar(int id, string model, string color, string manfacture)
+        {
+            Car cartest = new Car();
+            cartest.Num = id;
+            cartest.Model= model;
+            cartest.Color=color;
+            cartest.Manfacture= manfacture;
+
+            //CarList.cars.Insert(id, cartest);
+
+
+            CarList.cars.Add(cartest);
+
+            return RedirectToAction("GetAllCars");
+
+        }
+    }
+}
---
+++ MVC/Day2/CarOperation/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarOperation.Models
+{
+    public class Car
+    {
+        public int Num { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public string Manfacture { get; set; }
+
+
+    }
+}
