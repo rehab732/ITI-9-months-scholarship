c5ae9d110615e6b9b8886b6aad5d495b945c20d0
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-03-07 09:04:03 +0200

    add task2 for Day2 MVC

---
+++ MVC/Day2/ImageForm/Controllers/ImageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageForm.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+
+        public ActionResult ImageData()
+        {
+
+            return View();
+        }
+        public ActionResult ViewDetails(int id,string name,string imgnum)
+        {
+            ViewBag.id = id;
+            ViewBag.name = name;
+            
+            ViewBag.imgnum = "/Images/" + imgnum?.ToString() ;
+            return View();
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
