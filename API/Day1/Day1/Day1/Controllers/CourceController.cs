e0f39fde48738666164214d2c92331ad8f31f2b0
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-03-19 14:33:20 +0200

    API Day1

---
+++ API/Day1/Day1/Day1/Controllers/CourceController.cs
@@ -0,0 +1,69 @@
+﻿using Day1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Day1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourceController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            if(Course.Cources.Any()) return Ok(Course.Cources);
+            return NotFound();
+        }
+        [HttpDelete]
+        public IActionResult DeleteCource(int id)
+        {
+            /* If i return Cource List
+            Course c = Course.Cources.Find(co=>co.Id==id);
+            Course.Cources.Remove(c);
+            return Course.Cources.ToList();*/
+            var cource = Course.Cources.Where(co=>co.Id==id).FirstOrDefault();
+            if(cource==null) return NotFound();
+            Course.Cources.Remove(cource);
+            return Ok(Course.Cources);
+
+        }
+
+
+        [HttpPut]
+        public IActionResult Put(int id, Course course)
+        {
+            if(id!=course.Id) return BadRequest();
+            var c = Course.Cources.Where(c=>c.Id==id).FirstOrDefault(); 
+            if(c==null) return NotFound();
+            Course.Cources.Remove(c);
+            Course.Cources.Add(course);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Course course)
+        {
+            var c = Course.Cources.Where(c => c.Id == course.Id).FirstOrDefault();
+            if(course==null||c!=null) return BadRequest();
+            Course.Cources.Add(course);
+            return Created($"/GetById/{course.Id}", course);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var c = Course.Cources.Where(c => c.Id == id).FirstOrDefault();
+            if(c==null) return NotFound();  
+            return Ok(c);
+
+        }
+        [HttpGet("{name:alpha}")]
+        public IActionResult CourcseByName(string name)
+        {
+            var c = Course.Cources.Where(c => c.Name.ToLower() == name.ToLower()).FirstOrDefault();
+            if (c == null) return NotFound();
+            return Ok(c);
+
+        }
+    }
+}
---
+++ API/Day1/Day1/Day1/Models/Cource.cs
@@ -0,0 +1,22 @@
+﻿namespace Day1.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+
+
+        public static List<Course> Cources = new()
+        {
+
+
+            new Course() {Id = 1 , Name ="C" , Duration= 60 , Description ="intro to .net and c# Basics" },
+            new Course() {Id = 2 , Name ="SQL" , Duration=50 , Description ="Structure Query Language" },
+            new Course() {Id = 3 , Name ="Asp.net" , Duration= 36 , Description ="Server Side Programming" },
+            new Course() {Id = 4 , Name ="MVC" , Duration= 30 , Description ="Server Side Programming" },
+        };
+
+    }
+}
