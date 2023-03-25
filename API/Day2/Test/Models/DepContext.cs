f8037f57432f5b1e7aabbe8ca9d05efafb060b23
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-03-25 09:08:55 +0200

    API Day2

---
+++ API/Day2/Test/Models/DepContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Test.Models
+{
+    public class DepContext:DbContext
+    {
+        public DepContext()
+        {
+            //for create my owne object
+        }
+        public DepContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ API/Day2/Test/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace Test.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manager { get; set; }
+
+    }
+}
