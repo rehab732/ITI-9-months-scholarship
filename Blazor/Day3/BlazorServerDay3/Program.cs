3ea4a225b4124d5b2c027808c8c9667277e5f006
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-05-01 04:48:57 +0300

    finish identity

--- Blazor/Day3/BlazorServerDay3/Program.cs
+++ Blazor/Day3/BlazorServerDay3/Program.cs
@@ -2,6 +2,8 @@ using BlazorServerDay3.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorServerDay3
 {
@@ -10,8 +12,14 @@ namespace BlazorServerDay3
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("AuthContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthContextConnection' not found.");
+
+            builder.Services.AddDbContext<AuthContext>(options => options.UseSqlServer(connectionString));
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AuthContext>();
 
             // Add services to the container.
+            builder.Services.AddAuthentication("Identity.Application").AddCookie();
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
@@ -32,6 +40,9 @@ namespace BlazorServerDay3
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
